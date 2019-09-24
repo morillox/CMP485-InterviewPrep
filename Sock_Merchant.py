8b03adae2072a19ce412627596b5d0cea4a57bc6
Juan Morillo <morillox@hotmail.com>
2019-09-24 19:55:40 -0400

    File added or fixed.

--- Sock_Merchant.py
+++ Sock_Merchant.py
@@ -1,11 +1,28 @@
-from collections import Counter
-import math
-import os
-import random
-import re
-import sys
+"""
+John works at a clothing store. He has a large pile of socks that he must pair by color for sale.
+Given an array of integers representing the color of each sock, determine how many pairs of socks
+with matching colors there are.
 
-# Complete the sockMerchant function below.
+For example, there are n = 7 socks with colors ar = [1, 2, 1, 2, 1, 3, 2] . There is one pair of
+color 1 and one of color 2. There are three odd socks left, one of each color. The number of pairs is 2.
+"""
+
+
+from collections import Counter
+
+"""
+Task:
+
+Complete the sockMerchant function in the editor below. It must return an integer representing 
+the number of matching pairs of socks that are available. Create 3 test cases.
+
+sockMerchant has the following parameter(s):
+
+n: the number of socks in the pile
+ar: the colors of each sock
+"""
+
+
 def sockMerchant(n, ar):
     count = Counter(ar)
     pairs = 0
@@ -15,16 +32,20 @@ def sockMerchant(n, ar):
         else:
             pairs += ((count[key]-1)/2)
     return int(pairs)
-    
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    n = int(input())
 
-    ar = list(map(int, input().rstrip().split()))
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# =        Test cases           =
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-    result = sockMerchant(n, ar)
 
-    fptr.write(str(result) + '\n')
+test1 = [2, 2, 3, 3, 4, 4, 1]
+assert sockMerchant(7, test1) == 3, "PASS"
 
-    fptr.close()
+test2 = [2, 5, 3, 3, 4, 4, 1]
+assert sockMerchant(7, test2) == 2, "PASS"
+
+test3 = [2, 2, 3, 3, 4, 4, 1, 2, 2, 3, 3, 4, 4]
+assert sockMerchant(7, test3) == 6, "PASS"
+
+print("All test cases passed.")
